aed1660a844234d06af35f6b698b37ce4911f7a6
Ariel Levi Simons <levisimons@gmail.com>
2018-06-01 16:52:12 -0700

    Update PhyloSCCWRP.R

--- PhyloSCCWRP.R
+++ PhyloSCCWRP.R
@@ -9,6 +9,11 @@ library(data.table)
 library(tidyr)
 library("phyloseq")
 library(lattice)
+library(reshape2)
+library(geosphere)
+library(RADanalysis)
+library(MASS)
+library(fitdistrplus)
 
 setwd("~/Desktop/SCCWRP")
 
@@ -103,8 +108,8 @@ BMIData <- tmp1
 names(BMIData)[names(BMIData)=="BAResult"]<-"Count"
 
 #Select taxa group for downstream Phyloseq work and network generation.
-otudata <- benthicAlgaeData
-OTUID <- as.data.frame(benthicAlgaeID)
+otudata <- BMIData
+OTUID <- as.data.frame(BMIID)
 colnames(OTUID) <- c("FinalID")
 
 #Read in CSCI and land usage data by sample.
@@ -115,17 +120,31 @@ GISData <- subset(GISData,GISData$UniqueID %in% unique(otudata$UniqueID))
 GISData <- GISData[!duplicated(GISData$UniqueID),]
 
 #Add in aggregated land coverage at a 5km watershed buffer.
-GISData$LU_2000_5K <- with(GISData,Ag_2000_5K+CODE_21_2000_5K+URBAN_2000_5K)
+GISData$LU_2000_5K <- with(GISData,as.double(Ag_2000_5K+CODE_21_2000_5K+URBAN_2000_5K))
+#Aggregate land usage into deciles for downstream analysis.
+GISData$LU_Decile <- with(GISData, as.integer(LU_2000_5K/10))
+#Add in sample year as a variable
+GISData$Year <- year(mdy(GISData$SampleDate))
 
 #Final step to ensure that both the factors and biological data have the same set of sample IDs.
 otudata <- subset(otudata,otudata$UniqueID %in% unique(GISData$UniqueID))
 
+#Generate a diversity metric dataframe.
+#siteDiversity <- data.frame(nrow=1,ncol=2)
+#colnames(siteDiversity) <- c("UniqueID","Simpson")
+#tmp2 <- data.frame(nrow=1,ncol=2)
+#colnames(tmp2) <- c("UniqueID","Simpson")
+
 #Generate an OTU table of SCCWRP data for use in Phyloseq.
+#Generate diversity metrics per sample.
 for(sample in unique(otudata$UniqueID)){
   tmp1 <- subset(otudata,UniqueID==sample)
   tmp1 <- join(OTUID,tmp1,by=c("FinalID"))[,c("FinalID","Count")]
   OTUID <- cbind(OTUID,tmp1$Count)
   names(OTUID)[names(OTUID)=="tmp1$Count"]<-sample
+  #tmp2$UniqueID <- sample
+  #tmp2$Simpson <- diversity(subset(tmp1$Count,tmp1$Count > 0),index="simpson")
+  #siteDiversity <- rbind(siteDiversity,tmp2)
   print(sample)
 }
 
@@ -141,18 +160,38 @@ TAX = tax_table(taxmat)
 samplemat <- as.matrix(GISData)
 row.names(samplemat) <- GISData$UniqueID
 sampledata <- sample_data(as.data.frame(samplemat))
+sampledata$LU_2000_5K <- as.numeric(as.character(sampledata$LU_2000_5K))
+sampledata$LU_Decile <- as.numeric(as.character(sampledata$LU_Decile))
 physeq <- phyloseq(OTU,TAX,sampledata)
 
-test<-subset_samples(physeq)
+#Subset Phyloseq object by various factors and perform basic PCA and beta diversity tests.
+test <- subset_samples(physeq, Year=="2000")
+#test <- subset_samples(test, LU_Decile==1)
 #test <- transform_sample_counts(test, function(x) x/sum(x))
-test<-tax_glom(test,taxrank=rank_names(test)[4],NArm=TRUE,bad_empty=NA)
-
+#test<-tax_glom(test,taxrank=rank_names(test)[4],NArm=TRUE,bad_empty=NA)
 Dist = distance(test, method = "morisita")
 ord = ordinate(test, method = "PCoA", distance = Dist)
 plot_scree(ord, "Scree Plot: Bray-Curtis MDS")
-
+#Plot beta diversity.
 levelplot(as.matrix(Dist))
 hist(as.vector(as.matrix(Dist)))
-#Write out merged data set to read back in the future as opposed to 
-#generating it each time from component data files.
-#write.csv(GISBioData,file="CAGISBioData.csv",row.names=FALSE)
+
+#Convert beta diversity matrix into a three column data frame.
+distmat <- setNames(melt(as.matrix(Dist)), c("Site1","Site2","DiversityDistance"))
+#Merge in the spatial coordinates for each pair of sites.
+coord1 <- as.data.frame(as.matrix(sampledata[,c("UniqueID","Latitude","Longitude")]))
+rownames(coord1) <- 1:nrow(coord1)
+colnames(coord1) <- c("Site1","Lat1","Lon1")
+distmat <- merge(distmat,coord1,by=c("Site1"))
+coord2 <- as.data.frame(as.matrix(sampledata[,c("UniqueID","Latitude","Longitude")]))
+rownames(coord2) <- 1:nrow(coord2)
+colnames(coord2) <- c("Site2","Lat2","Lon2")
+distmat <- merge(distmat,coord2,by=c("Site2"))
+distmat$Lat1 <- as.numeric(as.character(distmat$Lat1))
+distmat$Lon1 <- as.numeric(as.character(distmat$Lon1))
+distmat$Lat2 <- as.numeric(as.character(distmat$Lat2))
+distmat$Lon2 <- as.numeric(as.character(distmat$Lon2))
+#Add in geodesic distances to compare separation distance to beta diversity distance.
+distmat$SpatialDistance <- lapply(1:nrow(distmat),function(x) as.numeric((distm(as.matrix(distmat[x,c("Lon1","Lat1")]), as.matrix(distmat[x,c("Lon2","Lat2")]), fun = distGeo))))
+distmat$SpatialDistance <- as.numeric(distmat$SpatialDistance)
+plot(distmat$SpatialDistance,distmat$DiversityDistance)
