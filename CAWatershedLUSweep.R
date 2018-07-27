c07e434cb4d16c47eb56f56532d048e4d1e063e7
Ariel Levi Simons <levisimons@gmail.com>
2018-07-27 08:40:14 -0700

    Update CAWatershedLUSweep.R

--- CAWatershedLUSweep.R
+++ CAWatershedLUSweep.R
@@ -31,7 +31,89 @@ for(WS in watersheds$Watershed){
   GISBioDataSubsetLow <- GISBioDataSubset[GISBioDataSubset$UniqueID %in% as.vector(unique(GISBioDataSubset$UniqueID)[1:groupNum]),]
   GISBioDataSubsetHigh <- GISBioDataSubset[GISBioDataSubset$UniqueID %in% as.vector(unique(GISBioDataSubset$UniqueID)[as.integer((sampleNum-groupNum)+1):as.integer(sampleNum)]),]
   for(i in 1:2){
-    if(i==1){selected <- GISBioDataSubsetLow}
-    if(i==2){selected <- GISBioDataSubsetHigh}
+    if(i==1){
+      lowNum = 1
+      highNum = groupNum
+      selected <- GISBioDataSubsetLow
+      meanLU_2000_5K = mean(na.omit(GISBioDataSubsetLow$LU_2000_5K))
+      }
+    if(i==2){
+      lowNum = as.integer((sampleNum-groupNum)+1)
+      highNum = as.integer(sampleNum)
+      selected <- GISBioDataSubsetHigh
+      meanLU_2000_5K = mean(na.omit(GISBioDataSubsetHigh$LU_2000_5K))
+      }
+    eLSAInput <- as.data.frame(unique(selected$FinalID))
+    colnames(eLSAInput)<-c("FinalID")
+    eLSAInput <- as.data.frame(eLSAInput[order(as.character(eLSAInput$FinalID)),])
+    colnames(eLSAInput)<-c("FinalID")
+    
+    #Add the relative taxa abundances by column to a new dataframe.
+    #The rows are the unique taxa in a given subset of data.
+    selected <- selected[order(selected$Year,selected$UniqueID,selected$FinalID),]
+    for(ID in unique(selected$UniqueID)){
+      tmp <- filter(selected, UniqueID == ID)[,c("FinalID","Measurement","UniqueID")]
+      tmp <- as.data.frame(tmp[order(tmp$FinalID),])
+      tmp <- tmp[-c(3)]
+      colnames(tmp)<-c("FinalID",paste("Measurement",ID,sep=" "))
+      eLSAInput <- join(eLSAInput,tmp,by="FinalID")
+      eLSAInput$FinalID=as.character(eLSAInput$FinalID)
+      eLSAInput <- eLSAInput %>% group_by(FinalID) %>% summarise_if(is.numeric,mean,na.rm=TRUE)
+    }
+    eLSAInput[is.na(eLSAInput)] <- "NA"
+    
+    #Determine the number of time points in the eLSA input file.
+    spotNum = length(unique(selected$Year))
+    #Determine the number of replicates per time point in the eLSA input file.
+    #In order to ensure a uniform number of replicates per year this needs to
+    #be the maximum number of replicates for all of the years available.
+    repMax = 0
+    for(year in unique(selected$Year)){
+      tmp <- filter(selected, Year == year)[,c("UniqueID","Year")]
+      repNum = length(unique(tmp$UniqueID))
+      if(repNum >= repMax){repMax = repNum}
+      #print (paste(repMax,repNum,year,sep=" "))
+    }
+    repNum = repMax
+    
+    #Now insert the replicates with actual data in between the "NA" dummy columns
+    #which ensure that the final eLSA input file has an even number of replicates
+    #per year regardless of the variations in the actual number of sites (replicates)
+    #sampled per year.
+    eLSAtmp <- eLSAInput[,1]
+    j=1
+    k=1
+    nulCol <- data.frame(matrix(ncol=repNum*spotNum,nrow=length(unique(selected$FinalID))))
+    nulCol[,1] <- eLSAInput[,1]
+    for(year in unique(selected$Year)){
+      tmp <- filter(selected, Year == year)
+      rep = length(unique(tmp$UniqueID))
+      for(i in 1:repNum){
+        if(i <= rep){
+          repLabel = paste(year,"DoneRep",i,sep="")
+          j=j+1
+          k=k+1
+          eLSAtmp[,k] <- eLSAInput[,j]
+        }
+        else{
+          repLabel = as.character(paste(year,"Rep",i,sep=""))
+          k=k+1
+          eLSAtmp[,k] <- "NA"
+          #print(paste(k,repLabel,sep=" "))
+        }
+      }
+    }
+    
+    eLSAInput <- eLSAtmp
+    #Designate a unique filename.
+    #S is the number of spots, or years represented in the subsample group.
+    #R is the number of replicates per year.  Many of the years will have null replicates, but a uniform number is needed for eLSA.
+    #M is the mean LU_2000_5K score per subsample group.
+    filename = paste("LUSweepCAWatershed",gsub(" ","",WS,fixed=TRUE),groupNum,"Samples",lowNum,"to",highNum,"S",spotNum,"R",repNum,"M",meanLU_2000_5K,sep="")
+    #Output file for use in eLSA.
+    write.table(eLSAInput,paste(filename,".txt",sep=""),quote=FALSE,sep="\t",row.names = FALSE)
+    eLSACommand = paste("lsa_compute ",filename,".txt ","-r ",repNum," -s ",spotNum," ",filename,"Network.txt;",sep="")
+    print(eLSACommand)
   }
-  }
+}
+
