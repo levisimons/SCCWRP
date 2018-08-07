d52f11be4e5b78be48383c07200041f682510950
Ariel Levi Simons <levisimons@gmail.com>
2018-08-07 15:53:33 -0700

    Update ZetaSCCWRP.R

--- ZetaSCCWRP.R
+++ ZetaSCCWRP.R
@@ -19,17 +19,33 @@ setwd("~/Desktop/SCCWRP")
 GISBioData <- read.table("CAGISBioData.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
 #Filter out to taxonomic groups of interest.
 GISBioData <- subset(GISBioData, MeasurementType == "benthic macroinvertebrate relative abundance")
+#Remove duplicate measures.
+GISBioData <- GISBioData[!duplicated(GISBioData[,c("UniqueID","FinalID","Count")]),]
 #Order data by LU_2000_5K.
 GISBioData <- arrange(GISBioData,LU_2000_5K)
+#Add taxa counts by sample.
+tmp <- data.frame(table(GISBioData$UniqueID))
+colnames(tmp) <- c("UniqueID","nTaxa")
+GISBioData <- join(GISBioData,tmp,by=c("UniqueID"))
 #Read in sample metadata.
 SCCWRP <- read.table("CSCI.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
+#Merge in sample altitude.
+GISBioData <- join(GISBioData,SCCWRP[,c("UniqueID","altitude")],by=c("UniqueID"))
 #Get samples per watershed.
 watersheds <- as.data.frame((table(SCCWRP$Watershed)))
 colnames(watersheds) <- c("Watershed","Samples")
+#Taxa frequency table.
+taxaFreq <- as.data.frame(table(GISBioData$FinalID))
+colnames(taxaFreq) <- c("FinalID","Freq")
+#Find the total number of taxa in the full data set.
+taxaMax <- length(unique(GISBioData$FinalID))
 
 #Generating a presence/absence matrix for California SCCWRP data.
-#selected <- subset(GISBioData,Watershed=="LosAngeles")
 selected <- GISBioData
+selected <- subset(GISBioData,LU_2000_5K>=90)
+#selected <- subset(selected,Year==2000)
+#selected <- subset(selected,LU_2000_5K>=20)
+#selected <- GISBioData
 selected <- arrange(selected,Year,UniqueID)
 eLSAInput <- as.data.frame(unique(selected$FinalID))
 colnames(eLSAInput)<-c("FinalID")
@@ -67,10 +83,10 @@ colnames(data.SCCWRP) <- eLSANames
 data.SCCWRP[data.SCCWRP > 0] <- 1
 
 #Store presence/absence matrix of California SCCWRP data.
-write.table(data.SCCWRP,"SCCWRPCAPresenceAbsence.txt",quote=FALSE,sep="\t",row.names = FALSE)
+#write.table(data.SCCWRP,"SCCWRPCAPresenceAbsence.txt",quote=FALSE,sep="\t",row.names = FALSE)
 
 #If the presence/absence matrix already exists load it here.
-data.SCCWRP <- read.table("SCCWRPCAPresenceAbsence.txt",header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
+#data.SCCWRP <- read.table("SCCWRPCAPresenceAbsence.txt",header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
 
 #Subset location data.
 xy.SCCWRP <- selected[,c("Latitude","Longitude","UniqueID")]
@@ -78,17 +94,46 @@ xy.SCCWRP <- xy.SCCWRP[!duplicated(xy.SCCWRP[c("UniqueID")]),]
 xy.SCCWRP <- xy.SCCWRP[,c("Latitude","Longitude")]
 
 #Subset environmental factor data.
-env.SCCWRP <- selected[,c("UniqueID","LU_2000_5K","Year")]
+env.SCCWRP <- selected[,c("UniqueID","Watershed","Ag_2000_5K","CODE_21_2000_5K","URBAN_2000_5K","LU_2000_5K","Year")]
 env.SCCWRP <- env.SCCWRP[!duplicated(env.SCCWRP[c("UniqueID")]),]
-env.SCCWRP <- env.SCCWRP[,c("LU_2000_5K","Year")]
+env.SCCWRP <- env.SCCWRP[,c("Watershed","Ag_2000_5K","CODE_21_2000_5K","URBAN_2000_5K","LU_2000_5K","Year")]
 env.SCCWRP$Year <- as.numeric(env.SCCWRP$Year)
+env.SCCWRP$Watershed <- as.factor(env.SCCWRP$Watershed)
 
 #Plotting zeta diversity decay with distance.
 Zeta.ddecay(xy.SCCWRP,data.SCCWRP,order=2,sam=nrow(data.SCCWRP),distance.type="ortho",plot=TRUE)
 
 #Zeta diversity with respect to environmental variables.
-Zeta.msgdm(data.SCCWRP,env.SCCWRP,xy=NULL,order=2)
+Zeta.msgdm(data.SCCWRP,env.SCCWRP,xy=NULL,sam=1000,order=2)
 
 #Computes zeta diversity, the number of species shared by multiple assemblages, for a range of orders (number of assemblages or sites), 
 #using combinations of sampled sites, and fits the decline to an exponential and a power law relationship.
 Zeta.decline.mc(data.SCCWRP,xy=NULL,orders=1:10,sam=1000)
+
+#Clustering samples into groups based on maximizing overlap in taxa present.
+library(ade4)
+comDist <- dist.binary(data.SCCWRP,method=1,diag=FALSE,upper=FALSE)
+comClust <- hclust(comDist,method="complete")
+comGroups <- cutree(comClust,h=0.8)
+comGroups <- as.data.frame(comGroups)
+data.SCCWRP.subset <- merge(data.SCCWRP,comGroups,by="row.names",all.x=TRUE)
+data.SCCWRP.subset <- subset(data.SCCWRP.subset,comGroups==1)
+dev.off()
+plot(as.dendrogram(comClust),horiz=TRUE)
+rect.hclust(comClust,h=0.6)
+
+library(proxy)
+library(dendextend)
+labels.SCCWRP <- selected[,c("UniqueID","Watershed","Ag_2000_5K","CODE_21_2000_5K","URBAN_2000_5K","LU_2000_5K","altitude","nTaxa","Latitude","Longitude")]
+labels.SCCWRP <- labels.SCCWRP[!duplicated(labels.SCCWRP[c("UniqueID")]),]
+labels.SCCWRP <- data.frame(labels.SCCWRP[,-1],row.names=labels.SCCWRP[,1])
+colnames(labels.SCCWRP) <- c("Watershed","Ag_2000_5K","CODE_21_2000_5K","URBAN_2000_5K","LU_2000_5K","altitude","nTaxa","Latitude","Longitude")
+data.SCCWRP.tagged <- merge(data.SCCWRP,labels.SCCWRP,by="row.names")
+
+#MDS plotting of Jaccard similarity between samples.
+mds <- cmdscale(dist(data.SCCWRP,method="binary"), eig=TRUE,k=2)
+mdsPlot <- data.frame(mds$points, group=data.SCCWRP.tagged$Latitude)
+#For continuous factors.
+ggplot(mdsPlot)+geom_point(aes(x = X1,y = X2, color = group))+ scale_colour_gradientn(colours=rainbow(4))
+#For discrete factors.
+ggplot(mdsPlot)+geom_point(aes(x = X1,y = X2, color = group))+ theme(legend.position="none")
