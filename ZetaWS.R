9aa79a72d8b2367425a2bc6e42db22a31d5dc224
Ariel Levi Simons <levisimons@gmail.com>
2018-08-17 10:57:29 -0700

    Update ZetaWS.R

--- ZetaWS.R
+++ ZetaWS.R
@@ -7,6 +7,8 @@ library("vegan")
 library("microbiome")
 library(data.table)
 library(tidyr)
+library(MASS)
+library(zetadiv)
 
 #Check for co-occurrence frequencies by watershed in the SCCWRP data set.
 setwd("~/Desktop/SCCWRP")
@@ -20,48 +22,127 @@ GISBioData <- GISBioData[!duplicated(GISBioData[,c("UniqueID","FinalID","Count")
 SCCWRP <- read.table("CSCI.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
 #How many samples per watershed?
 groupNum=20
-#Run through analysis on SCCWRP archive on a watershed-level scale.
+
+#Look through watersheds which are sufficiently sampled and do the following:
+#Divide the entire data set, composed of heavily sampled watersheds, into quantiles based on land use.
+#Calculate zeta diversity of each land use band.
+#Determine the histogram of the number of watersheds each co-occurrence occurs in.
+#Calculate the parameters of the gamma distribution fit to this histogram.
 #Select watersheds with a large enough set of samples for analysis.
+zetaAnalysis <- data.frame()
 watersheds <- subset(as.data.frame(table(SCCWRP$Watershed)),Freq>=groupNum)
 colnames(watersheds) <- c("Watershed","Samples")
 #Get samples only found in more heavily sampled watersheds.
 GISBioDataLargeWS <- subset(GISBioData,Watershed %in% watersheds$Watershed)
-#Subset samples by aggregated land use.
-GISBioDataLargeWS <- subset(GISBioDataLargeWS,LU_2000_5K<=5)
-#Which watersheds are heavily sampled and meet a land use criterion?
-watersheds <- subset(watersheds,Watershed %in% GISBioDataLargeWS$Watershed)
-
-#Get the frequency of pairs of taxa showing up by watershed.
-#For example, a pair showing up three times in one watershed, and eight times in a second watershed,
-#will be counted as having shown up in two unique watersheds.
-CAMatch <- data.frame()
-for(WS in watersheds$Watershed){
-  WSSubset <- subset(GISBioDataLargeWS,Watershed==WS)
-  WSMatch <- data.frame()
-  for(ID in unique(WSSubset$UniqueID)){
-    WSSample <- subset(WSSubset,UniqueID==ID)
-    #Subset samples by aggregated land use.
-    if(length(unique(WSSample$FinalID))>2){
-      taxaMatch <- as.data.frame(t(combn(unique(WSSample$FinalID),2)))
-      WSMatch <- rbind(WSMatch,taxaMatch)
-      WSMatch <- WSMatch[!duplicated(WSMatch[,c("V1","V2")]),]
-      print(paste(WS,ID,length(unique(WSSample$FinalID))))
-    }
+LUquantile <- quantile(GISBioDataLargeWS$LU_2000_5K,probs=seq(0,1,0.1))#To get land use quantiles.
+for(i in 1:length(LUquantile)){
+  watersheds <- subset(as.data.frame(table(SCCWRP$Watershed)),Freq>=groupNum)
+  colnames(watersheds) <- c("Watershed","Samples")
+  #Get samples only found in more heavily sampled watersheds.
+  GISBioDataLargeWS <- subset(GISBioData,Watershed %in% watersheds$Watershed)
+  LULow <- as.numeric(LUquantile[i])
+  if(i<length(LUquantile)){
+    LUHigh <- as.numeric(LUquantile[i+1])
   }
-  WSName <- data.frame(matrix(nrow=nrow(WSMatch),ncol=1))
-  colnames(WSName) <- c("Watershed")
-  WSName$Watershed <- WS
-  WSMatch <- cbind(WSMatch,WSName)
-  CAMatch <- rbind(CAMatch,WSMatch)
+  if(i==length(LUquantile)){
+    LUHigh == 100
+  }
+  if(LULow == 0 & LUHigh == 0){
+    GISBioDataLargeWS <- subset(GISBioDataLargeWS,LU_2000_5K==LULow) #Subset samples by aggregated land use.
+    #print(paste(i-1,i,LULow,LUHigh,length(unique(GISBioDataLargeWS$UniqueID))))
+  }
+  if(LULow != LUHigh){
+    GISBioDataLargeWS <- subset(GISBioDataLargeWS,LU_2000_5K>=LULow & LU_2000_5K < LUHigh) #Subset samples by aggregated land use.
+    #print(paste(i-1,i,LULow,LUHigh,length(unique(GISBioDataLargeWS$UniqueID))))
+  }
+  if(i < length(LUquantile)){
+    selected <- GISBioDataLargeWS
+    #Get zeta diversity decay parameters for the same set of samples within a given land use band.
+    selected <- arrange(selected,Year,UniqueID)
+    eLSAInput <- as.data.frame(unique(selected$FinalID))
+    colnames(eLSAInput)<-c("FinalID")
+    eLSAInput <- as.data.frame(eLSAInput[order(as.character(eLSAInput$FinalID)),])
+    colnames(eLSAInput)<-c("FinalID")
+    taxa <- eLSAInput
+    
+    #Add the relative taxa abundances by column to a new dataframe.
+    #The rows are the unique taxa in a given subset of data.
+    selected <- selected[order(selected$Year,selected$UniqueID,selected$FinalID),]
+    i=0
+    for(ID in unique(selected$UniqueID)){
+      tmp <- filter(selected, UniqueID == ID)[,c("FinalID","Measurement","UniqueID")]
+      tmp <- as.data.frame(tmp[order(tmp$FinalID),])
+      tmp <- tmp[-c(3)]
+      colnames(tmp)<-c("FinalID",ID)
+      tmp <- tmp %>% group_by(FinalID) %>% summarise_if(is.numeric,mean,na.rm=TRUE)
+      tmp <- join(tmp,taxa,type="full",by=c("FinalID"))
+      tmp <- as.data.frame(tmp[order(tmp$FinalID),])
+      eLSAInput <- cbind(eLSAInput,tmp)
+      eLSAInput <- eLSAInput[,!duplicated(colnames(eLSAInput))]
+    }
+    
+    #Generate a presence/absence dataframe for zeta diversity analysis.
+    #Rows for samples, columns for taxa IDs.
+    eLSAInput[is.na(eLSAInput)] <- 0
+    eLSANames <- eLSAInput$FinalID
+    data.SCCWRP <- as.data.frame(t(eLSAInput[,-c(1)]))
+    colnames(data.SCCWRP) <- eLSANames
+    data.SCCWRP[data.SCCWRP > 0] <- 1
+    
+    #Computes zeta diversity, the number of species shared by multiple assemblages, for a range of orders (number of assemblages or sites), 
+    #using combinations of sampled sites, and fits the decline to an exponential and a power law relationship.
+    zetaDecay <- Zeta.decline.mc(data.SCCWRP,xy=NULL,orders=1:10,sam=1000)
+    dat <- data.frame()
+    dat[1,1] <- zetaDecay$zeta.exp$coefficients[1] #Zeta diversity exponential decay intercept.
+    dat[1,2] <- zetaDecay$zeta.exp$coefficients[2] #Zeta diversity exponential decay exponent.
+    dat[1,3] <- zetaDecay$aic$AIC[1] #AIC coefficient Zeta diversity exponential decay.
+    dat[1,4] <- zetaDecay$zeta.pl$coefficients[1] #Zeta diversity power law decay intercept.
+    dat[1,5] <- zetaDecay$zeta.pl$coefficients[2] #Zeta diversity power law decay exponent.
+    dat[1,6] <- zetaDecay$aic$AIC[2] #AIC coefficient Zeta diversity power law decay.
+    
+    #Get the frequency of pairs of taxa showing up by watershed.
+    #For example, a pair showing up three times in one watershed, and eight times in a second watershed,
+    #will be counted as having shown up in two unique watersheds.
+    CAMatch <- data.frame()
+    #Which watersheds are heavily sampled and meet a land use criterion?
+    watersheds <- subset(watersheds,Watershed %in% GISBioDataLargeWS$Watershed)
+    for(WS in watersheds$Watershed){
+      WSSubset <- subset(GISBioDataLargeWS,Watershed==WS)
+      WSMatch <- data.frame()
+      for(ID in unique(WSSubset$UniqueID)){
+        WSSample <- subset(WSSubset,UniqueID==ID)
+        #Subset samples by aggregated land use.
+        if(length(unique(WSSample$FinalID))>2){
+          taxaMatch <- as.data.frame(t(combn(unique(WSSample$FinalID),2)))
+          WSMatch <- rbind(WSMatch,taxaMatch)
+          WSMatch <- WSMatch[!duplicated(WSMatch[,c("V1","V2")]),]
+          print(paste(WS,ID,length(unique(WSSample$FinalID))))
+        }
+      }
+      WSName <- data.frame(matrix(nrow=nrow(WSMatch),ncol=1))
+      colnames(WSName) <- c("Watershed")
+      WSName$Watershed <- WS
+      WSMatch <- cbind(WSMatch,WSName)
+      CAMatch <- rbind(CAMatch,WSMatch)
+    }
+    CAMatch <- ddply(CAMatch, .(CAMatch$V1,CAMatch$V2),nrow)
+    colnames(CAMatch) <- c("V1","V2","NumWS")
+    hist(CAMatch$NumWS,xlim=c(0,63),ylim=c(0,40000))
+    histDecay <- fitdist(CAMatch$NumWS,"gamma",method="mle")
+    dat[1,7] <- as.numeric(histDecay$estimate[1]) #Gamma distribution histogram fit shape parameter.
+    dat[1,8] <- as.numeric(histDecay$sd[1]) #Gamma distribution histogram fit shape parameter standard error.
+    dat[1,9] <- as.numeric(histDecay$estimate[2])#Gamma distribution histogram fit rate parameter.
+    dat[1,10] <- as.numeric(histDecay$sd[2]) #Gamma distribution histogram fit rate parameter standard error.
+    print(dat)
+  }
+  zetaAnalysis <- rbind(zetaAnalysis,dat)
 }
-CAMatch <- ddply(CAMatch, .(CAMatch$V1,CAMatch$V2),nrow)
-colnames(CAMatch) <- c("V1","V2","NumWS")
-#write.table(CAMatch,"AssociationsPerWaterhsedLUlt5.txt",quote=FALSE,sep="\t",row.names = FALSE)
-CAMatch <- read.table("AssociationsPerWaterhsedLUgt15.txt",header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
-hist(CAMatch$NumWS,xlim=c(0,63),ylim=c(0,40000))
-library(MASS)
-fitdist(CAMatch$NumWS,"gamma",method="mle")
+colnames(zetaAnalysis) <- c("zetaExpIntercept","zetaExpExponent","zetaExpAIC","zetaPLIntercept","zetaPLExponent","zetaPLAIC","GammaShapeParameter","GammaShapeSE","GammaRateParameter","GammaRateSE")
 
+
+
+
+#####################################################################
 
 #Run through analysis on SCCWRP archive on a watershed-level scale.
 #Select watersheds with a large enough set of samples for analysis.
@@ -70,7 +151,6 @@ colnames(watersheds) <- c("Watershed","Samples")
 #Get samples only found in more heavily sampled watersheds.
 GISBioDataLargeWS <- subset(GISBioData,Watershed %in% watersheds$Watershed)
 
-library(zetadiv)
 #Read in site data containing biological counts, water chemistry, and land usage
 #values.  If this file is not yet generated then proceed with the following commands
 #to generate it in the first place.
