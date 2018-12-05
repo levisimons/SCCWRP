55c09f324dbe8fed0e8b0bd32bfd1cdbe84a56f5
Ariel Levi Simons <levisimons@gmail.com>
2018-12-05 12:42:52 -0800

    Update NetworkTopologyAnalysis.R

--- NetworkTopologyAnalysis.R
+++ NetworkTopologyAnalysis.R
@@ -11,6 +11,17 @@ chart.Correlation(networkAnalysis[,c("meanLU","zeta_Cov","l_cov_rL","l_cov_rM","
 chart.Correlation(networkAnalysis[,c("meanLU","zeta_Con","l_con_rL","l_con_rM","con_C","meanStrength_Con","Pm","lambda_network_m","Network_size")], histogram=FALSE, method="spearman")
 chart.Correlation(networkAnalysis[,c("meanLU","zeta","l_rM","M","lambda_network_m","Network_size")], histogram=FALSE, method="spearman")
 
-TopologyModel <- lm(lambda_network_m ~ meanLU+M+cov_C+con_C+l_cov_rL+l_con_rL+meanStrength_Cov+meanStrength_Con+Pm+Network_size+zeta_Cov+zeta_Con, data=networkAnalysis)
+#Testing models relating network parameters
+TopologyModel <- lm(lambda_network_m ~ con_M+cov_M+cov_C+con_C+l_cov_rL+l_con_rL+meanStrength_Cov+meanStrength_Con+Pm+Network_size+zeta_Cov+zeta_Con, data=networkAnalysis)
 summary(TopologyModel)
+printCoefmat(coef(summary(step(TopologyModel)))) #Determine parameters to drop from model via AIC scores.
 calc.relimp(TopologyModel,type="lmg",rela=TRUE)
+
+#Run through correlations between upstream land use and topology
+varList <- c("meanLU","Pm","Network_size","cov_C","con_C","cov_M","con_M","meanStrength_Cov","meanStrength_Con","zeta_Cov","zeta_Con","l_cov_rL","l_con_rL")
+for(var in varList){
+  topologyCorrelation <- cor.test(networkAnalysis$meanLU,networkAnalysis[,var])
+  resilienceCorrelation <- cor.test(networkAnalysis$lambda_network_m,networkAnalysis[,var])
+  print(paste("meanLU and",var,topologyCorrelation$estimate,topologyCorrelation$p.value))
+  print(paste("lambda_network_m and",var,resilienceCorrelation$estimate,resilienceCorrelation$p.value))
+}
