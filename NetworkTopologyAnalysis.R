a11ce19bba3387a8e482eeb59651a1de4df54f5d
Ariel Levi Simons <levisimons@gmail.com>
2018-12-10 10:26:34 -0800

    Update NetworkTopologyAnalysis.R

--- NetworkTopologyAnalysis.R
+++ NetworkTopologyAnalysis.R
@@ -19,9 +19,18 @@ calc.relimp(TopologyModel,type="lmg",rela=TRUE)
 
 #Run through correlations between upstream land use and topology
 varList <- c("meanLU","Pm","Network_size","cov_C","con_C","cov_M","con_M","meanStrength_Cov","meanStrength_Con","zeta_Cov","zeta_Con","l_cov_rL","l_con_rL")
+networkCorrelations <- data.frame()
 for(var in varList){
-  topologyCorrelation <- cor.test(networkAnalysis$meanLU,networkAnalysis[,var])
-  resilienceCorrelation <- cor.test(networkAnalysis$lambda_network_m,networkAnalysis[,var])
+  topologyCorrelation <- cor.test(networkAnalysis$meanLU,networkAnalysis[,var],method="spearman")
+  resilienceCorrelation <- cor.test(networkAnalysis$lambda_network_m,networkAnalysis[,var],method="spearman")
   print(paste("meanLU and",var,topologyCorrelation$estimate,topologyCorrelation$p.value))
   print(paste("lambda_network_m and",var,resilienceCorrelation$estimate,resilienceCorrelation$p.value))
+  dat <- data.frame()
+  dat[1,1] <- var
+  dat[1,2] <- topologyCorrelation$estimate
+  dat[1,3] <- topologyCorrelation$p.value
+  dat[1,4] <- resilienceCorrelation$estimate
+  dat[1,5] <- resilienceCorrelation$p.value
+  networkCorrelations <- rbind(networkCorrelations,dat)
 }
+colnames(networkCorrelations) <- c("Variable","CorrelationWithMeanLU","pCorrelationWithMeanLU","CorrelationWithResilience","pCorrelationWithResilience")
