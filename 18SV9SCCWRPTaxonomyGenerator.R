b2fa5c6d9f3cb02a4a582fd3d448e00f670a316c
Ariel Levi Simons <levisimons@gmail.com>
2020-01-29 14:25:18 -0800

    Update 18SV9SCCWRPTaxonomyGenerator.R

--- 18SV9SCCWRPTaxonomyGenerator.R
+++ 18SV9SCCWRPTaxonomyGenerator.R
@@ -6,14 +6,13 @@ require(tidyr)
 require(naniar)
 require(taxize)
 
-#wd <- "/home/cmb-07/sn1/alsimons/SCCWRP"
+options(ENTREZ_KEY="3f9e29c7c03842f72cf7523e34390d9f2208")
 wd <- "~/Desktop/SCCWRP/Metagenomics/"
 setwd(wd)
 
-##Only run this once to generate full taxonomy files for algal and BMI samples.
 #Read in metagenomic count tables and format them as presence/absence tables.
-communityInputRawPlate1 <- read.table("18SV9P1TableWithTaxonomy.txt", header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,quote="",check.names=FALSE, encoding = "UTF-8")
-communityInputRawPlate2 <- read.table("18SV9P2TableWithTaxonomy.txt", header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,quote="",check.names=FALSE, encoding = "UTF-8")
+communityInputRawPlate1 <- read.table("18SV9P1TableWithTaxonomy.txt", header=T, sep="\t",as.is=T,skip=0,fill=TRUE,quote="",check.names=FALSE, encoding = "UTF-8")
+communityInputRawPlate2 <- read.table("18SV9P2TableWithTaxonomy.txt", header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,quote="",check.names=F, encoding = "UTF-8")
 #Remove spaces from column names for the count tables.
 communityInputRawPlate1 <- dplyr::rename(communityInputRawPlate1, OTUID = `OTU ID`)
 communityInputRawPlate2 <- dplyr::rename(communityInputRawPlate2, OTUID = `OTU ID`)
@@ -28,9 +27,8 @@ uniqueOTUs$ConsensusLineage <- gsub("D_[0-9]+__","",uniqueOTUs$ConsensusLineage)
 uniqueOTUs$ConsensusLineage <- gsub("g__","",uniqueOTUs$ConsensusLineage)
 #Split OTU names into Domain through Genus+Species.
 uniqueOTUs$FullTaxonomy <- uniqueOTUs$ConsensusLineage
-uniqueOTUs <- suppressWarnings(separate(uniqueOTUs,'ConsensusLineage',c("Rank1", "Rank2","Rank3","Rank4","Rank5","Rank6","Rank7and8"),sep=";", extra="drop"))
-uniqueOTUs$Rank7and8 <- trimws(uniqueOTUs$Rank7and8,which="left") #Remove starting blank space from genus names
-uniqueOTUs <- suppressWarnings(separate(uniqueOTUs,'Rank7and8',c("Rank7","Rank8"),sep=" ", extra="warn"))
+uniqueOTUs <- suppressWarnings(separate(uniqueOTUs,'ConsensusLineage',c("Rank1", "Rank2","Rank3","Rank4","Rank5","Rank6","Rank7"),sep=";", extra="drop"))
+uniqueOTUs$Rank7 <- trimws(uniqueOTUs$Rank7,which="left") #Remove starting blank space from genus names
 uniqueOTUs <- uniqueOTUs[!duplicated(uniqueOTUs$OTUID),]
 #Filter out ambiguous taxonomies
 uniqueOTUs <- uniqueOTUs[uniqueOTUs$Rank1!="Unassigned" & uniqueOTUs$Rank1!="Ambiguous_taxa",]
@@ -45,11 +43,12 @@ uniqueOTUs$LeafTaxa <- apply(uniqueOTUs[,!colnames(uniqueOTUs) %in% c("FullTaxon
 BMIList <- read.table("bug_STE.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,quote="",check.names=FALSE, encoding = "UTF-8",na.strings=c("","NA"))
 BMIList <- unique(BMIList$FamilyNames)
 BMIList <- na.omit(BMIList)
+
 BMITaxonomies <- data.frame()
 i=0
 for(name in BMIList){
-  tmp <- classification(name,db="gbif",rows=1)
-  Sys.sleep(0.5)
+  tmp <- classification(name,db="ncbi",rows=1)
+  #Sys.sleep(0.5)
   if(nrow(as.data.frame(tmp[1]))>1){
     tmp <- as.data.frame(tmp[1])
     colnames(tmp) <- c("taxa","rank","id")
@@ -59,24 +58,43 @@ for(name in BMIList){
       colnames(tmp) <- as.character(unlist(tmp["rank",]))
       tmp <- tmp[!row.names(tmp) %in% c("rank"),]
       rownames(tmp) <- name
+      tmp$LeafTaxa <- name
       BMITaxonomies <- dplyr::bind_rows(BMITaxonomies,tmp)
       i=i+1
       print(paste("BMIs",i,length(BMIList)))
+    } else{
+      tmp <- classification(name,db="gbif",rows=1)
+      #Sys.sleep(0.5)
+      if(nrow(as.data.frame(tmp[1]))>1){
+        tmp <- as.data.frame(tmp[1])
+        colnames(tmp) <- c("taxa","rank","id")
+        tmp <- tmp[tmp$rank!="no rank",]
+        if(nrow(tmp)>1){
+          tmp <- as.data.frame(t(tmp[,c("taxa","rank")]))
+          colnames(tmp) <- as.character(unlist(tmp["rank",]))
+          tmp <- tmp[!row.names(tmp) %in% c("rank"),]
+          rownames(tmp) <- name
+          tmp$LeafTaxa <- name
+          BMITaxonomies <- dplyr::bind_rows(BMITaxonomies,tmp)
+          i=i+1
+          print(paste("BMIs",i,length(BMIList)))
+        }
+      }
     }
   }
 }
 
 #Subset 18Sv9 OTU table to only contain taxonomic BMI data from SCCWRP
 BMITaxonomies <- na.omit(unique(unlist(BMITaxonomies)))
+BMITaxonomies <- BMITaxonomies[!grepl("\\d",BMITaxonomies)] #Remove numerical taxa terms.
 uniqueBMIs <- uniqueOTUs[grep(paste(BMITaxonomies,collapse="|"),uniqueOTUs$FullTaxonomy),]
 #
 BMIList <- na.omit(unique(uniqueBMIs$LeafTaxa))
 BMITaxonomies <- data.frame()
 i=0
-for(j in 1:nrow(uniqueBMIs)){
-  name <- as.character(uniqueBMIs[j,"LeafTaxa"])
-  tmp <- classification(name,db="gbif",rows=1)
-  Sys.sleep(0.5)
+for(name in BMIList){
+  tmp <- classification(name,db="ncbi",rows=1)
+  #Sys.sleep(0.5)
   if(nrow(as.data.frame(tmp[1]))>1){
     tmp <- as.data.frame(tmp[1])
     colnames(tmp) <- c("taxa","rank","id")
@@ -86,25 +104,47 @@ for(j in 1:nrow(uniqueBMIs)){
       colnames(tmp) <- as.character(unlist(tmp["rank",]))
       tmp <- tmp[!row.names(tmp) %in% c("rank"),]
       rownames(tmp) <- name
-      tmp <- cbind(uniqueBMIs[j,],tmp)
+      tmp$LeafTaxa <- name
       BMITaxonomies <- dplyr::bind_rows(BMITaxonomies,tmp)
       i=i+1
       print(paste("BMIs",i,length(BMIList)))
+    } else{
+      tmp <- classification(name,db="gbif",rows=1)
+      #Sys.sleep(0.5)
+      if(nrow(as.data.frame(tmp[1]))>1){
+        tmp <- as.data.frame(tmp[1])
+        colnames(tmp) <- c("taxa","rank","id")
+        tmp <- tmp[tmp$rank!="no rank",]
+        if(nrow(tmp)>1){
+          tmp <- as.data.frame(t(tmp[,c("taxa","rank")]))
+          colnames(tmp) <- as.character(unlist(tmp["rank",]))
+          tmp <- tmp[!row.names(tmp) %in% c("rank"),]
+          rownames(tmp) <- name
+          tmp$LeafTaxa <- name
+          BMITaxonomies <- dplyr::bind_rows(BMITaxonomies,tmp)
+          i=i+1
+          print(paste("BMIs",i,length(BMIList)))
+        }
+      }
     }
   }
 }
+BMITaxonomies <- dplyr::left_join(BMITaxonomies,uniqueBMIs,by=c("LeafTaxa"))
+BMITaxonomies <- BMITaxonomies[BMITaxonomies$kingdom=="Metazoa",]
 BMITaxonomies <- BMITaxonomies[c("OTUID","kingdom","phylum","class","order","family","genus","species")]
+
 write.table(BMITaxonomies,"BMITaxonomies18SV9.txt",quote=FALSE,sep="\t",row.names = FALSE)
 
 #Read in SCCWRP algal taxonomic phyla-level ids in order to subset read data and focus analyses on just algae.
 AlgaeList <- read.table("algae_STE.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,quote="",check.names=FALSE, encoding = "UTF-8",na.strings=c("","NA"))
 AlgaeList <- na.omit(unique(AlgaeList$Phylum))
 AlgaeList <- na.omit(AlgaeList)
+AlgaeList <- AlgaeList[!(AlgaeList %in% c("Eukaryota","Cyanobacteria","Bacteria"))]
 AlgalTaxonomies <- data.frame()
 i=0
 for(name in AlgaeList){
-  tmp <- classification(name,db="gbif",rows=1)
-  Sys.sleep(0.5)
+  tmp <- classification(name,db="ncbi",rows=1)
+  #Sys.sleep(0.5)
   if(nrow(as.data.frame(tmp[1]))>1){
     tmp <- as.data.frame(tmp[1])
     colnames(tmp) <- c("taxa","rank","id")
@@ -114,27 +154,43 @@ for(name in AlgaeList){
       colnames(tmp) <- as.character(unlist(tmp["rank",]))
       tmp <- tmp[!row.names(tmp) %in% c("rank"),]
       rownames(tmp) <- name
+      tmp$LeafTaxa <- name
       AlgalTaxonomies <- dplyr::bind_rows(AlgalTaxonomies,tmp)
       i=i+1
       print(paste("Algae",i,length(AlgaeList)))
+    } else{
+      tmp <- classification(name,db="gbif",rows=1)
+      #Sys.sleep(0.5)
+      if(nrow(as.data.frame(tmp[1]))>1){
+        tmp <- as.data.frame(tmp[1])
+        colnames(tmp) <- c("taxa","rank","id")
+        tmp <- tmp[tmp$rank!="no rank",]
+        if(nrow(tmp)>1){
+          tmp <- as.data.frame(t(tmp[,c("taxa","rank")]))
+          colnames(tmp) <- as.character(unlist(tmp["rank",]))
+          tmp <- tmp[!row.names(tmp) %in% c("rank"),]
+          rownames(tmp) <- name
+          tmp$LeafTaxa <- name
+          AlgalTaxonomies <- dplyr::bind_rows(AlgalTaxonomies,tmp)
+          i=i+1
+          print(paste("Algae",i,length(AlgaeList)))
+        }
+      }
     }
   }
 }
 
 #Subset 18Sv9 OTU table to only contain taxonomic algal data from SCCWRP
 AlgalTaxonomies <- na.omit(unique(unlist(AlgalTaxonomies)))
-tmp <- c("Eukaryota","Bacteria","Cyanobacteria")
-AlgalTaxonomies <-  AlgalTaxonomies[!AlgalTaxonomies %in% tmp]
-#plantOTUs <- uniqueOTUs[uniqueOTUs$kingdom %in% c("Archaeplastida","Cryptophyceae","Haptophyta"),]
+AlgalTaxonomies <- AlgalTaxonomies[!grepl("\\d",AlgalTaxonomies)]
 uniqueAlgae <- uniqueOTUs[grep(paste(AlgalTaxonomies,collapse="|"),uniqueOTUs$FullTaxonomy),]
 #
 AlgaeList <- na.omit(unique(uniqueAlgae$LeafTaxa))
-AlgaeTaxonomies <- data.frame()
+AlgalTaxonomies <- data.frame()
 i=0
-for(j in 1:nrow(uniqueAlgae)){
-  name <- as.character(uniqueAlgae[j,"LeafTaxa"])
-  tmp <- classification(name,db="gbif",rows=1)
-  Sys.sleep(0.5)
+for(name in AlgaeList){
+  tmp <- classification(name,db="ncbi",rows=1)
+  #Sys.sleep(0.5)
   if(nrow(as.data.frame(tmp[1]))>1){
     tmp <- as.data.frame(tmp[1])
     colnames(tmp) <- c("taxa","rank","id")
@@ -144,12 +200,33 @@ for(j in 1:nrow(uniqueAlgae)){
       colnames(tmp) <- as.character(unlist(tmp["rank",]))
       tmp <- tmp[!row.names(tmp) %in% c("rank"),]
       rownames(tmp) <- name
-      tmp <- cbind(uniqueAlgae[j,],tmp)
-      AlgaeTaxonomies <- dplyr::bind_rows(AlgaeTaxonomies,tmp)
+      tmp$LeafTaxa <- name
+      AlgalTaxonomies <- dplyr::bind_rows(AlgalTaxonomies,tmp)
       i=i+1
       print(paste("Algae",i,length(AlgaeList)))
+    } else{
+      tmp <- classification(name,db="gbif",rows=1)
+      #Sys.sleep(0.5)
+      if(nrow(as.data.frame(tmp[1]))>1){
+        tmp <- as.data.frame(tmp[1])
+        colnames(tmp) <- c("taxa","rank","id")
+        tmp <- tmp[tmp$rank!="no rank",]
+        if(nrow(tmp)>1){
+          tmp <- as.data.frame(t(tmp[,c("taxa","rank")]))
+          colnames(tmp) <- as.character(unlist(tmp["rank",]))
+          tmp <- tmp[!row.names(tmp) %in% c("rank"),]
+          rownames(tmp) <- name
+          tmp$LeafTaxa <- name
+          AlgalTaxonomies <- dplyr::bind_rows(AlgalTaxonomies,tmp)
+          i=i+1
+          print(paste("Algae",i,length(AlgaeList)))
+        }
+      }
     }
   }
 }
-AlgaeTaxonomies <- AlgaeTaxonomies[c("OTUID","kingdom","phylum","class","order","family","genus","species")]
-write.table(AlgaeTaxonomies,"AlgaeTaxonomies18SV9.txt",quote=FALSE,sep="\t",row.names = FALSE)
+AlgalTaxonomies[] <- lapply(AlgalTaxonomies,as.character)
+AlgalTaxonomies <- AlgalTaxonomies[!AlgalTaxonomies$kingdom %in% c("Chromista","Metazoa"),]
+AlgalTaxonomies <- dplyr::left_join(AlgalTaxonomies,uniqueAlgae,by=c("LeafTaxa"))
+AlgalTaxonomies <- AlgalTaxonomies[c("OTUID","kingdom","phylum","class","order","family","genus","species")]
+write.table(AlgalTaxonomies,"AlgaeTaxonomies18SV9.txt",quote=FALSE,sep="\t",row.names = FALSE)
